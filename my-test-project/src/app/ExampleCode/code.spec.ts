d7eb79057fa92e0f3a92707962c7e986cb6eedd2
joe99peace <jjcusey@gmail.com>
2019-02-16 13:21:08 -0500

    add examples

--- my-test-project/src/app/ExampleCode/code.spec.ts
+++ my-test-project/src/app/ExampleCode/code.spec.ts
@@ -1,10 +1,63 @@
-import {compute} from './code';
+import { Fundamentals } from './code';
+import { ArraysAndStrings } from './code';
+import { SetupAndTeardownComponent } from './code';
 
-describe('compute', () => {
-    it('should return 0 if input is negative', () => {
-        const result = compute(-1);
-        console.log('Compute(-1) = ' + result);
+describe('Fundamentals', () => {
+    it('compute(-1) :should return 0 if input is negative', () => {
+        const job = new Fundamentals();
+        const result = job.compute(-1);
+        console.log('compute(-1) = ' + result);
         expect(result).toBe(0);
     });
+});
 
+describe('ArraysAndStrings', () => {
+    it('greet(\'cusey\') :should include the name in the message', () => {
+        const job = new ArraysAndStrings();
+        const result = job.greet('cusey');
+        console.log('greet(\'cusey\') = ' + result);
+        expect(result).toContain('cusey');
+    });
+
+    it('getCurrencies()  :should return the supported currencies', () => {
+        const job = new ArraysAndStrings();
+        const result = job.getCurrencies();
+        console.log('getCurrencies() = ' + result);
+        expect(result).toContain('USD');
+        expect(result).toContain('AUD');
+        expect(result).toContain('EUR');
+    });
+
+});
+
+describe('SetupAndTeardownComponent', () => {
+    let component;
+
+    beforeEach(() => {
+        // Set up
+        component = new SetupAndTeardownComponent();
+
+    });
+    afterEach(() => {
+        // tear down
+    });
+
+    it('upVotes(): should increment totalVotes when upvoted', () => {
+        // Arrange
+
+        // Act
+        component.upVote();
+        // Assert
+        expect(component.totalVotes).toBe(1);
+
+    });
+
+    it('upVotes(): should decrement totalVotes when downvoted', () => {
+        // Arrange
+        // Act
+        component.downVote();
+        // Assert
+        expect(component.totalVotes).toBe(-1);
+
+    });
 });
--- my-test-project/src/app/ExampleCode/code.ts
+++ my-test-project/src/app/ExampleCode/code.ts
@@ -1,6 +1,38 @@
-export function compute(number: any) {
-    if (number < 0) {
-        return 0;
+// 01 Fundamentals
+export class Fundamentals {
+
+    compute(number: any) {
+        if (number < 0) {
+            return 0;
+        }
+        return number + 1;
     }
-    return number + 1;
+
+}
+// 02 Arrays And Strings
+export class ArraysAndStrings {
+
+    greet(name) {
+        return 'Welcome ' + name;
+    }
+
+    getCurrencies() {
+        return ['USD', 'AUD', 'EUR'];
+    }
+}
+
+
+
+
+// 03 Setup And Teardown
+export class SetupAndTeardownComponent {
+    totalVotes = 0;
+
+    upVote() {
+       this.totalVotes++;
+    }
+
+    downVote() {
+       this.totalVotes--;
+    }
 }
